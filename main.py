dbadb6e7c6371040df6de1e1cc84bc8a6a839fc3
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-01-29 21:25:10 +0100

    Add age validation to determine if user is an adult or infant

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
+age = input("How old are you? ")
 
-a = int(input("Enter a: "))
-b = int(input("Enter b: "))
-c = int(input("Enter c: "))
-
-P = a + b + c
-p = P / 2
-S = math.sqrt(p * (p - a) * (p - b) * (p - c))
-
-print("P = ", P)
+if int(age) >= 18:
+    print("You are adult already.")
+else:
+    print("You are infant yet.")
