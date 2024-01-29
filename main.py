2e96150edf4d437a30b72d962a1e49dac169e7fe
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-01-29 22:06:13 +0100

    Add input validation for x to prevent division by zero

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-some_data = None
-msg = some_data or "No data returned"
-print(msg)
+x = int(input("x: "))
+y = int(input("y: "))
+
+if x == 0:
+    print("X cannot be 0")
+    x = int(input("x: "))
+        
+        if x == 0:
+            print("X cannot be 0")
+            x = int(input("x: "))
+
+                if x == 0:
+                    print("X cannot be 0")
+                    x = int(input("x: "))
+
+result = y / x
