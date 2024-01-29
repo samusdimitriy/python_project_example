cccbd9a1cffd9d3cb6de1018c8dba64c89294cfc
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-01-29 21:32:03 +0100

    Add user input for name and greet user accordingly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-money = 0
-if money:
-    print(f"You have {money} on your bank account")
-else: 
-    print("You have no money on your bank account")
+user_name = input("Enter your name: ")
+
+if user_name:
+    print(f"Hello {user_name}")
+else:
+    print("Hi Anonym!")
