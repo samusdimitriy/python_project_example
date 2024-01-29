029e809f602cbe230aa9fbfcd7ef17d29d97332d
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-01-29 22:08:54 +0100

    Refactor input validation and add fruit loop

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-x = int(input("x: "))
-y = int(input("y: "))
-
-if x == 0:
-    print("X cannot be 0")
-    x = int(input("x: "))
-        
-        if x == 0:
-            print("X cannot be 0")
-            x = int(input("x: "))
-
-                if x == 0:
-                    print("X cannot be 0")
-                    x = int(input("x: "))
-
-result = y / x
+fruit = 'apple'
+for char in fruit:
+    print(char)
