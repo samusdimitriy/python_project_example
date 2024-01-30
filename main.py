57a7326cd36d5523466591102e6e915fdebf180f
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-01-30 13:12:54 +0100

    Refactor user access logic to calculate developer type based on work experience

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-'''
-У нас есть три логические переменные.
+work_experience = int(input("Enter your full work experience in years: "))
 
-Первая определяет статус пользователя is_active, которая равна True или False.
-Вторая is_admin определяет, есть ли у пользователя права администратора, булевого типа.
-Третья is_permission — разрешен ли доступ, тоже булевого типа.
-Приведите переменные is_active, is_admin и is_permission к булевому виду.
-
-Присвойте переменной access значение, которое покажет, есть ли доступ у пользователя. Используйте логические операторы.
-
-Администратор всегда имеет доступ, независимо от значений переменных is_permission и is_active.
-
-Пользователь имеет доступ, только если is_permission равен True и is_active также равен True.
-'''
-
-is_active = bool(input("Is the user active? "))
-is_admin = bool(input("Is the user administrator? "))
-is_permission = bool(input("Does the user have access? "))
-
-access = is_admin or is_active and is_permission
-
+if work_experience > 1 and work_experience <= 5:
+    developer_type = "Middle"
+elif work_experience <= 1:
+    developer_type = "Junior"
+else:
+    developer_type = "Senior"
