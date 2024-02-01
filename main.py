999719809d0086badb3b91aa7de27284e3b7ef6f
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-02-01 14:04:52 +0100

    Refactor message encoding logic

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-sum = 0
-while True:
-    num = int(input("Enter integer (0 for output): "))
-    if num == 0:
-        break
+message = input("Enter a message: ")
+offset = int(input("Enter the offset: "))
+encoded_message = ""
+for ch in message:
+    if ch.isalpha():
+        if ch.islower():
+            pos = ord(ch) - ord('a')
+            pos = (pos + offset) % 26
+            new_char = chr(pos + ord('a'))
+            encoded_message += new_char
+        else:
+            pos = ord(ch) - ord('A')
+            pos = (pos + offset) % 26
+            new_char = chr(pos + ord('A'))
+            encoded_message += new_char
+    else:
+        encoded_message += ch
         
-    for i in range(num + 1):
-        sum = sum + i
-print(sum)
+print(encoded_message)
