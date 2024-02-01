9e6b5c2a2c9c0571ba751173b2b342d5a7f86f8d
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-02-01 14:07:41 +0100

    Refactor encoding logic and add error handling for zero division

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-message = input("Enter a message: ")
-offset = int(input("Enter the offset: "))
-encoded_message = ""
-for ch in message:
-    if ch.isalpha():
-        if ch.islower():
-            pos = ord(ch) - ord('a')
-            pos = (pos + offset) % 26
-            new_char = chr(pos + ord('a'))
-            encoded_message += new_char
-        else:
-            pos = ord(ch) - ord('A')
-            pos = (pos + offset) % 26
-            new_char = chr(pos + ord('A'))
-            encoded_message += new_char
-    else:
-        encoded_message += ch
-        
-print(encoded_message)
+pool = 1000
+quantity = int(input("Enter the number of mailings: "))
+try:
+    chunk = pool // quantity
+except ZeroDivisionError:
+    print('Divide by zero completed!')
