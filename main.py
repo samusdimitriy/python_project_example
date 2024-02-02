e305cf381075e34da8e26a0c9b15551cd4c61b0b
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-02-02 21:09:25 +0100

    Refactor login logic to use a for loop for countdown

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-username = input("Enter your user name please: ")
-password = input("Enter your user password please: ")
-
-if "admin" in username:
-    if password == "qwerty":
-        print(f"Login successful! Welcome {username}")
-    elif password == "12345":
-        print("Week password")
-    else:
-        print("incorrect password")
-elif "guest" in username:
-    if password == "guest123":
-        print(f"Login successful! Welcome {username}")
-    else:
-        print("incorrect password")
-else: 
-    print("only guests or admins allowed")
+for i in range(7, 1, -1):
+    print(i)
