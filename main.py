80fcc4e525fc573ed2c80d06ef4387ff7c136ffe
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-02-03 10:40:45 +0100

    Add message encoding functionality

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-age = 18
-is_allowed = False
-
-is_allowed = True if age >= 18 else False
+message = input("Enter a message: ")
+offset = int(input("Enter the offset: "))
+encoded_message = ""
+for ch in message:
+    if ' ' <= ch <= '`':
+        if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
+            encoded_message += ch
+            continue
+        pos = ord(ch) - ord(' ')
+        pos = (pos + offset) % 94
+        new_char = chr(pos + ord(" "))
+        encoded_message += new_char
