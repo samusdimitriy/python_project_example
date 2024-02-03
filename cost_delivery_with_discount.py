4738c44705565150fcd41ab496e92fd0d64aae01
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-02-03 16:10:32 +0100

    Refactor cost_delivery function to include a discount parameter

--- cost_delivery_with_discount.py
+++ cost_delivery_with_discount.py
@@ -1,5 +1,7 @@
-def cost_delivery(quantity, *_, discount = 0):
+def cost_delivery(quantity, *_, discount=0):
+    """Функция возвращает общую сумму доставки.
+
+    Первый параметр quantity количество товаров в заказе.
+    Параметр скидки discount, передаваемый только по ключу, по умолчанию имеет значение 0."""
     result = (5 + 2 * (quantity - 1)) * (1 - discount)
     return result
-
-print(cost_delivery(3,2,3,4))
