80d02b59e065a3ccaf07a5469c8cfd173bec18de
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-02-10 09:56:20 +0100

    Add function to convert camel case to snake case

--- textbook.py
+++ textbook.py
@@ -1,4 +1,20 @@
-import sys
 
-for arg in sys.argv:
-    print(arg)
+
+def convert_camel_to_snake(text):
+    j = 0
+    res = ""
+    for char in text:
+        if char.isupper():
+            if j == 0:
+                res += char.lower()
+            else:
+                res += "_" + char.lower()
+        else:
+            res += char
+        j += 1
+    return res  
+
+s = "HelloWorld"
+
+res = convert_camel_to_snake(s)
+print(res)
