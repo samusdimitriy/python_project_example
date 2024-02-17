df3c572af7c960df1cde03777a2eb7d614f9e224
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-02-17 16:08:19 +0100

    Fix bug in login functionality

---
+++ get_grade.py
@@ -0,0 +1,18 @@
+def get_grade(key):
+    dict = {"A": 5, "B": 5, "C": 4, "D": 3, "E": 3, "FX": 2, "F": 1}
+    return dict.get(key)
+
+
+def get_description(key):
+    dict = {"A": "Perfectly", "B": "Very good", "C": "Good", "D": "Satisfactorily", "E": "Enough", "FX": "Unsatisfactorily", "F": "Unsatisfactorily"}
+    return dict.get(key)
+
+        
+        
+        
+        
+        
+        
+        
+    
+    
